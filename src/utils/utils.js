b278794cc0bc337a4c0da2b120e44f5e87662ec5
Zack Lyon <zack.j.lyon@gmail.com>
2021-12-17 11:16:20 -0800

    Add testing, begin refactor to hooks file

---
+++ src/utils/utils.js
@@ -0,0 +1,23 @@
+import { makeBoard } from '../Boxes-utils.js';
+const clone = require('rfdc')();
+
+const fetchBoard = ({ setSolutionBoard, setShowingBoard }) => {
+  const newBoard = makeBoard();
+
+  //set up showing board so that squares that aren't prefilled have a value of ""
+  const showingBoard = newBoard.map((square) =>
+    square.prefilled === true ? square : { number: '', prefilled: false }
+  );
+
+  setSolutionBoard(newBoard);
+  setShowingBoard(showingBoard);
+};
+
+const makeCorrectArr = ({ solutionBoard, setCorrect }) => {
+  //make an array of booleans that has prefilled board squares marked as true and empty squares as false that can be altered by index as squares are filled correctly
+  const correctArr = solutionBoard.map((item) => item.prefilled);
+
+  setCorrect(correctArr);
+};
+
+export { fetchBoard, makeCorrectArr };
